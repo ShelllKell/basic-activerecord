36f766a5cf8809b4fab748ed7e0c11679fe0866d
Shelby Kelly <shelby.kelly@colorado.edu>
2014-07-28 12:01:33 -0600

    lunch time

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -60,13 +60,56 @@ customers.each do |cust|
   p cust
 end
 
+# p '*' * 20
+
+# customers = Item.exists?(['name LIKE ?', "boot"])
+# customers.each do |cust|
+#   p cust
+# end
+
 p '*' * 20
 
-customers = Item.where(id: [7,8,9])
+customers = Order.all
 customers.each do |cust|
   p cust
 end
 
 p '*' * 20
 
+customers = Order.first(5)
+customers.each do |cust|
+  p cust
+end
 
+p '*' * 20
+
+customers = Order.last(5)
+customers.each do |cust|
+  p cust
+end
+
+p '*' * 20
+
+customers = Customer.pluck(:name, :email)
+customers.each do |cust|
+  p cust
+end
+
+p '*' * 20
+
+customers = Customer.pluck(:id, :name, :address)
+customers.each do |cust|
+  p cust
+end
+
+p '*' * 20
+
+customers = Customer.count
+  p customers
+
+p '*' * 20
+
+customers = Order.sum(:amount)
+ p customers.to_s.to_f
+
+p '*' * 20
