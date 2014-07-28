b8f05006aad419bfb442cdf9b74f6dd76d9112ff
Shelby Kelly <shelby.kelly@colorado.edu>
2014-07-28 11:18:23 -0600

    Finished first row of stories

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -4,3 +4,51 @@ require "./lib/item"
 require "./lib/orderitem"
 require "./lib/order"
 
+customers = Customer.all
+
+customers.each do |customer|
+  puts customer.inspect
+end
+
+p '*' * 20
+
+first_customer = Customer.first
+p first_customer
+
+p '*' * 20
+
+last_customer = Customer.last
+p last_customer
+
+p '*' * 20
+
+cust_three = Customer.find(3)
+p cust_three
+
+p '*' * 20
+
+colorado = Customer.where(state: 'Colorado')
+colorado.each do |state|
+  puts state.inspect
+end
+
+p '*' * 20
+
+customers = Customer.first(5)
+customers.each do |cust|
+  puts cust.inspect
+end
+
+p '*' * 20
+
+customers = Customer.order(:name)
+customers.each do |cust|
+  puts cust.inspect
+end
+
+p '*' * 20
+
+customers = Customer.order(:name :desc)
+customers.each do |cust|
+  puts cust.inspect
+end
